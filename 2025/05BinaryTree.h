bfe6f7945be0932823abef9108c8ac4f877ecbe5
Goldemarie <74863466+ba-ya@users.noreply.github.com>
2025-10-26 19:04:06 +0800

    feat:  BinaryTree_Understand, 104,111

--- 2025/05BinaryTree.h
+++ 2025/05BinaryTree.h
@@ -13,7 +13,36 @@ struct TreeNode {
 };
 
 namespace BinaryTree_Understand {
+// 104二叉树的最大深度
+int maxDepth(TreeNode* root) {
+    if (root == nullptr) {
+        return 0;
+    }
+    int depth_l = maxDepth(root->left);
+    int depth_r = maxDepth(root->right);
+    return max(depth_l, depth_r) + 1;
+}
 
+// 111二叉树的最小深度
+int minDepth(TreeNode* root) {
+    // 与两个子节点进行比较
+    static int ans = INT_MAX;
+    std::function<void(TreeNode*, int)> dfs;
+    dfs = [&](TreeNode* root, int cnt) {
+        if (root == nullptr) {
+            return;
+        }
+        cnt++;
+        if (root->left == nullptr && root->right == nullptr) {
+            ans = min(ans, cnt);
+            return;
+        }
+        dfs(root->left, cnt);
+        dfs(root->right, cnt);
+    };
+    dfs(root, 0);
+    return root ? ans : 0;
+}
 }
 namespace BinaryTree_Apply {
 
